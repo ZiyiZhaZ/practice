2603a2840ec3d15d3a71816fa83c34e39e64c8e2
ziyi <zhaziyi@qq.com>
2023-04-25 20:33:13 +0800

    1

---
+++ 1.py
@@ -0,0 +1,23 @@
+print("Hello, I am Ziyi!")
+
+# TODO: Replace 'FILL_ME' with your own code
+first_name = "Ziyi Zha"
+print('Name: ' + first_name)
+# TODO: Repeat the above for pid and year
+pid = "A16347544"
+print('PID: ' + pid)
+year = 2020
+print('Year of starting school at UCSD: ' + str(year))
+# TODO: Calculate your academic year
+academic_year = 2022 - year + 1
+# TODO: Print your academic year
+print('Academic year:',academic_year)
+
+# TODO: prompt user to enter a float value. Hint: use input()
+x = input("Enter your float number: ")
+# TODO: convert the user input to a float. Hint: use float()
+y = float(x)
+# TODO: convert float value to integer. Hint: use int()
+z = int(y)
+# TODO: print the type-converted integer
+print("Type conversion to integer:", z)
